53c63a505a3b7e4157bb8d3bb9f1167e6c941769
terrypan1 <terrypan1@gmail.com>
2023-11-22 22:12:05 +0800

    更新 upade todo

--- src/components/Todo/TodoMain.vue
+++ src/components/Todo/TodoMain.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import useStore from "../../stores/dotoIndex";
-
+import { ITodoItem } from '../../types/data'
 const { main } = useStore()
-// console.log(main);
-const { getTodos } = main
+const { getTodos, delTodo, updateTodo } = main
 const { list } = storeToRefs(main)
 getTodos()
+const handleChangeInput = (item: ITodoItem) => {
+    console.log(item)
+    updateTodo(item.id, 'done', !item.done)
+}
 </script>
 <template>
     <section class="main">
@@ -17,9 +20,9 @@ getTodos()
             <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
             <li :class="{ completed: item.done }" v-for="item in list" :key="item.id">
                 <div class="view">
-                    <input class="toggle" type="checkbox" :checked="item.done">
+                    <input class="toggle" type="checkbox" :checked="item.done" @change="handleChangeInput(item)">
                     <label>{{ item.name }}</label>
-                    <button class="destroy"></button>
+                    <button class="destroy" @click="delTodo(item.id)"></button>
                 </div>
                 <input class="edit" value="Create a TodoMVC template">
             </li>
--- src/stores/Todo/mainStore.ts
+++ src/stores/Todo/mainStore.ts
@@ -9,10 +9,34 @@ const mainStore = defineStore('main', {
         }
     },
     actions: {
+        /**
+         * 獲取數據
+         */
         async getTodos() {
-            console.log(request)
-            const { data } = await request.get('/')
+            const { data } = await request.get<ITodoItem[]>('/')
             this.list = data
+        },
+        /**
+         * 刪除數據
+         * @param id 
+         */
+        async delTodo(id: number) {
+            console.log(id)
+            await request.delete(`/${id}`)
+            this.getTodos()
+        },
+        /**
+         * 更新數據
+         * @param id 根據id來更新
+         * @param key 更新誰
+         * @param value 更新值
+         */
+        async updateTodo(id: number, key: string, value: string | boolean) {
+            await request.patch(`/${id}`, {
+                [key]: value
+            })
+            console.log('123')
+            this.getTodos()
         }
     }
 })
