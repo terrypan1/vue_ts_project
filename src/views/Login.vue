61062e1aecd768adae011b70b31d1b9ee8737607
terrypan1 <terrypan1@gmail.com>
2023-12-14 19:18:44 +0800

    更新 首頁圖片載入

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -27,61 +27,61 @@ onMounted(() => {
 });
 </script>
 <template>
-    <!-- <div v-if="isImageLoaded"> -->
-    <div class="container-fuild h-100" style="background-color: white !important;">
-        <div class="row g-0 h-100 w-100">
-            <div class="col-lg-4 left-container d-none d-lg-block">
-                <div class="left-img">
+    <div v-if="isImageLoaded">
+        <div class="container-fuild h-100" style="background-color: white !important;">
+            <div class="row g-0 h-100 w-100">
+                <div class="col-lg-4 left-container d-none d-lg-block">
+                    <div class="left-img">
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-8 h-100">
-                <div class="row g-0">
-                    <div class="d-flex flex-column align-items-center justify-content-center" style="height: 100vh;">
-                        <div class="d-flex flex-column align-items-center right-container">
-                            <h1 class="fw-bold">Sign In</h1>
-                            <p class="fw-medium text-muted fw-bold">
-                                Welcome,Please log in or
-                                <a href="">
-                                    create a new account
-                                </a>
-                            </p>
-                            <div class="mb-4 mt-4">
-                                <input type="text" class="form-control form-control-alt" placeholder="Username"
-                                    :class="{ 'is-invalid': v$.username.$errors.length, }" v-model="state.username"
-                                    @blur="v$.username.$touch">
-                                <div v-if="v$.username.$errors.length" class="invalid-feedback">
-                                    請輸入使用者名稱
+                <div class="col-lg-8 h-100">
+                    <div class="row g-0">
+                        <div class="d-flex flex-column align-items-center justify-content-center" style="height: 100vh;">
+                            <div class="d-flex flex-column align-items-center right-container">
+                                <h1 class="fw-bold">Sign In</h1>
+                                <p class="fw-medium text-muted fw-bold">
+                                    Welcome,Please log in or
+                                    <a href="">
+                                        create a new account
+                                    </a>
+                                </p>
+                                <div class="mb-4 mt-4">
+                                    <input type="text" class="form-control form-control-alt" placeholder="Username"
+                                        :class="{ 'is-invalid': v$.username.$errors.length, }" v-model="state.username"
+                                        @blur="v$.username.$touch">
+                                    <div v-if="v$.username.$errors.length" class="invalid-feedback">
+                                        請輸入使用者名稱
+                                    </div>
+                                </div>
+                                <div class="mb-4">
+                                    <input type="password" class="form-control form-control-alt" placeholder="Password"
+                                        :class="{ 'is-invalid': v$.password.$errors.length }" v-model="state.password"
+                                        @blur="v$.password.$touch">
+                                    <div v-if="v$.password.$errors.length" class="invalid-feedback">
+                                        請輸入使用者密碼
+                                    </div>
+                                </div>
+                                <div class="mb-4 w-100">
+                                    <div class="d-flex justify-content-between ms-4 me-4">
+                                        <a style="text-decoration: none;font-size: 14px;color: rgb(84, 91, 98);"
+                                            class="fw-bold">Forgot Password?</a>
+                                        <button type="button" class="btn btn-primary login-btn" @click="onSubmit">
+                                            <i class="bi bi-box-arrow-right"></i>
+                                            sigh in
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="mb-4">
-                                <input type="password" class="form-control form-control-alt" placeholder="Password"
-                                    :class="{ 'is-invalid': v$.password.$errors.length }" v-model="state.password"
-                                    @blur="v$.password.$touch">
-                                <div v-if="v$.password.$errors.length" class="invalid-feedback">
-                                    請輸入使用者密碼
-                                </div>
-                            </div>
-                            <div class="mb-4 w-100">
-                                <div class="d-flex justify-content-between ms-4 me-4">
-                                    <a style="text-decoration: none;font-size: 14px;color: rgb(84, 91, 98);"
-                                        class="fw-bold">Forgot Password?</a>
-                                    <button type="button" class="btn btn-primary login-btn" @click="onSubmit">
-                                        <i class="bi bi-box-arrow-right"></i>
-                                        sigh in
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- </div> -->
-    <!-- <div v-else>
+    <div v-else>
         <div>Loading...</div>
-    </div> -->
+    </div>
 </template>
 <style lang="scss" scoped>
 .left-img {
