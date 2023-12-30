018397145d544187cad2ed7d24f3ed8be8b6f078
terrypan1 <terrypan1@gmail.com>
2023-12-30 12:37:06 +0800

    更新 carousel

--- src/views/Elements/Carousel.vue
+++ src/views/Elements/Carousel.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import { carouselHelpers } from './useHeadering';//使用BaseHeadering
 import { card } from './useBaseBlock';//BaseBlock
-import {ref,onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import Reload from '../../components/Login/Reload.vue'
 
 const isImageLoaded = ref(false);//定義一個數據屬性 來追蹤圖片是否加載完成
-const loadImage = () => {
+const loadImage = (src: string) => {
     return new Promise<void>((resolve) => {
-        const img1 = new Image()
-        img1.src = '/assets/imgs/carousel1.png';
-        img1.onload = () => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
             resolve();
         };
-    })
+    });
 }
 onMounted(async () => {
-    await Promise.all([loadImage()]);
+    // 等待所有圖片加載
+    await Promise.all([
+        loadImage('/assets/imgs/carousel1.png'),
+        loadImage('/assets/imgs/carousel2.png'),
+        loadImage('/assets/imgs/carousel3.png')
+    ]);
     isImageLoaded.value = true;
 });
 </script>
@@ -35,22 +40,25 @@ onMounted(async () => {
                             <div id="carouselExampleControls" class="carousel slide">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active img-container">
-                                        <img src="/assets/imgs/carousel1.png" class="d-block w-100" alt="Carousel Image 1" />
+                                        <img src="/assets/imgs/carousel1.png" class="d-block w-100"
+                                            alt="Carousel Image 1" />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="/assets/imgs/carousel2.png" class="d-block w-100" alt="Carousel Image 2" />
+                                        <img src="/assets/imgs/carousel2.png" class="d-block w-100"
+                                            alt="Carousel Image 2" />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="/assets/imgs/carousel3.png" class="d-block w-100" alt="Carousel Image 3" />
+                                        <img src="/assets/imgs/carousel3.png" class="d-block w-100"
+                                            alt="Carousel Image 3" />
                                     </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                    data-bs-slide="prev">
+                                <button class="carousel-control-prev" type="button"
+                                    data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                    data-bs-slide="next">
+                                <button class="carousel-control-next" type="button"
+                                    data-bs-target="#carouselExampleControls" data-bs-slide="next">
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
@@ -77,22 +85,25 @@ onMounted(async () => {
                                 </div>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img src="/assets/imgs/carousel3.png" class="d-block w-100" alt="Carousel Image 1" />
+                                        <img src="/assets/imgs/carousel3.png" class="d-block w-100"
+                                            alt="Carousel Image 1" />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="/assets/imgs/carousel2.png" class="d-block w-100" alt="Carousel Image 1" />
+                                        <img src="/assets/imgs/carousel2.png" class="d-block w-100"
+                                            alt="Carousel Image 1" />
                                     </div>
                                     <div class="carousel-item">
-                                        <img src="/assets/imgs/carousel1.png" class="d-block w-100" alt="Carousel Image 1" />
+                                        <img src="/assets/imgs/carousel1.png" class="d-block w-100"
+                                            alt="Carousel Image 1" />
                                     </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide="prev">
+                                <button class="carousel-control-prev" type="button"
+                                    data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                                    data-bs-slide="next">
+                                <button class="carousel-control-next" type="button"
+                                    data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
@@ -109,72 +120,46 @@ onMounted(async () => {
                         </template>
                         <template #content>
                             <div id="carouselExampleCaptions" class="carousel slide">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img
-                      src="/assets/imgs/carousel2.png"
-                      class="d-block w-100"
-                      alt="Carousel Image 1"
-                    />
-                    <div
-                      class="carousel-caption d-none d-md-block rounded-3 px-3" style="background-color: rgb(0,0,0,0.5);"
-                    >
-                      <h5 class="h3 mb-2">Terry Pan</h5>
-                      <p class="mb-0">Welcome to my profile</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <img
-                      src="/assets/imgs/carousel1.png"
-                      class="d-block w-100"
-                      alt="Carousel Image 2"
-                    />
-                    <div
-                      class="carousel-caption d-none d-md-block rounded-3 px-3" style="background-color: rgb(0,0,0,0.5);"
-                    >
-                      <h5 class="h3 mb-2">Terry Pan</h5>
-                      <p class="mb-0">Welcome to my profile</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <img
-                      src="/assets/imgs/carousel3.png"
-                      class="d-block w-100"
-                      alt="Carousel Image 3"
-                    />
-                    <div
-                      class="carousel-caption d-none d-md-block rounded-3 px-3" style="background-color: rgb(0,0,0,0.5);"
-                    >
-                      <h5 class="h3 mb-2">Terry Pan</h5>
-                      <p class="mb-0">Welcome to my profile</p>
-                    </div>
-                  </div>
-                </div>
-                <button
-                  class="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#carouselExampleCaptions"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    class="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button
-                  class="carousel-control-next"
-                  type="button"
-                  data-bs-target="#carouselExampleCaptions"
-                  data-bs-slide="next"
-                >
-                  <span
-                    class="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="/assets/imgs/carousel2.png" class="d-block w-100"
+                                            alt="Carousel Image 1" />
+                                        <div class="carousel-caption d-none d-md-block rounded-3 px-3"
+                                            style="background-color: rgb(0,0,0,0.5);">
+                                            <h5 class="h3 mb-2">Terry Pan</h5>
+                                            <p class="mb-0">Welcome to my profile</p>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/assets/imgs/carousel1.png" class="d-block w-100"
+                                            alt="Carousel Image 2" />
+                                        <div class="carousel-caption d-none d-md-block rounded-3 px-3"
+                                            style="background-color: rgb(0,0,0,0.5);">
+                                            <h5 class="h3 mb-2">Terry Pan</h5>
+                                            <p class="mb-0">Welcome to my profile</p>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/assets/imgs/carousel3.png" class="d-block w-100"
+                                            alt="Carousel Image 3" />
+                                        <div class="carousel-caption d-none d-md-block rounded-3 px-3"
+                                            style="background-color: rgb(0,0,0,0.5);">
+                                            <h5 class="h3 mb-2">Terry Pan</h5>
+                                            <p class="mb-0">Welcome to my profile</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button"
+                                    data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button"
+                                    data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
                         </template>
                     </BaseBlock>
                 </div>
