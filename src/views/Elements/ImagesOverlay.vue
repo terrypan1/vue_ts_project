b11aeb910603984ae5e22fe6db4cb39c4b9664a1
terrypan1 <terrypan1@gmail.com>
2023-12-30 12:32:49 +0800

    更新 carousel

--- src/views/Elements/ImagesOverlay.vue
+++ src/views/Elements/ImagesOverlay.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { helpers } from './useHeadering';//使用BaseHeadering
 import { card } from './useBaseBlock';//BaseBlock
+
 </script>
 <template>
     <BaseHeadering :header=helpers></BaseHeadering>
